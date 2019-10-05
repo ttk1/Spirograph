c47666d4691115417c0c9dd81f50d7427def4d3e
tama <tama@ttk1.net>
2019-10-05 20:09:03 +0900

    描画周りのたたき台

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,35 @@
-console.log('hello, world!');
+const WIDTH = 500;
+const HEIGHT = 500;
+const POINT_SIZE = 5;
+
+window.onload = () => {
+  const canvas = document.createElement('canvas');
+  canvas.width = WIDTH;
+  canvas.height = HEIGHT;
+  document.body.appendChild(canvas);
+  const context = canvas.getContext('2d');
+
+  refresh();
+  for (let i = 0; i < 100; i++) {
+    drawPoint(
+      Math.random() - 0.5,
+      Math.random() - 0.5
+    );
+  }
+
+  function drawPoint(x: number, y: number) {
+    context.fillStyle = 'black';
+    context.beginPath();
+    context.arc(
+      WIDTH * (x + 0.5),
+      HEIGHT * (y + 0.5),
+      POINT_SIZE, 0, 2 * Math.PI, true
+    );
+    context.fill();
+  }
+
+  function refresh() {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, WIDTH, HEIGHT);
+  }
+};
