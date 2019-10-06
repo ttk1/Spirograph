ed631953e57010d2bc28fc2c03440b677004faac
tama <tama@ttk1.net>
2019-10-07 01:30:12 +0900

    Tweakpaneを導入

---
+++ README.md
@@ -0,0 +1,7 @@
+# Spirograph
+
+* [スピログラフ](https://ja.wikipedia.org/wiki/%E3%82%B9%E3%83%94%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%95)
+
+## デモ
+
+* https://spirograph.ttk1.dev
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
+import TP from 'tweakpane';
+
 const POINT_SIZE = 1;
-const R = 300; // 外側の円の半径
-const r = 0.3; // 外側の円に対する、内側の円の半径の比
-const s = 0.8; // 内側の円の鉛筆を刺す位置（0が中心で、1が一番外側）
+const CANVAS_SIZE = 500;
+
+const PARAMS = {
+  R: 250, // 外側の円の半径
+  r: 0.3, // 外側の円に対する、内側の円の半径の比
+  s: 0.8  // 鉛筆を刺す位置（0が中心で、1が一番外側）
+};
 
 window.onload = () => {
-  const canvas = document.createElement('canvas');
-  canvas.width = 2 * R;
-  canvas.height = 2 * R;
-  document.body.appendChild(canvas);
+  const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+  canvas.width = CANVAS_SIZE;
+  canvas.height = CANVAS_SIZE;
   const context = canvas.getContext('2d');
+
+  const paneContainer = document.getElementById('pane');
+  paneContainer.style.width = '500px';
+  const Tweakpane = require('tweakpane');
+  const pane = new Tweakpane({
+    container: paneContainer,
+  }) as TP;
 
   refresh();
   let t = 0;
@@ -17,7 +29,40 @@ window.onload = () => {
     t += 0.01;
   }, 5);
 
+  pane.addInput(PARAMS, 'R', {
+    label: '外側の円の半径',
+    min: 1,
+    max: 250,
+    step: 1
+  }).on('change', () => {
+    t = 0;
+    refresh();
+  });
+
+  pane.addInput(PARAMS, 'r', {
+    label: '外側の円に対する、内側の円の半径の比',
+    min: 0,
+    max: 1,
+    step: 0.01
+  }).on('change', () => {
+    t = 0;
+    refresh();
+  });
+
+  pane.addInput(PARAMS, 's', {
+    label: '鉛筆を刺す位置（0が中心で、1が一番外側）',
+    min: 0,
+    max: 1,
+    step: 0.01
+  }).on('change', () => {
+    t = 0;
+    refresh();
+  });
+
   function drawPoint(theta: number) {
+    const R = PARAMS.R;
+    const r = PARAMS.r;
+    const s = PARAMS.s;
     context.fillStyle = 'black';
     context.beginPath();
     context.arc(
@@ -30,6 +75,6 @@ window.onload = () => {
 
   function refresh() {
     context.fillStyle = 'white';
-    context.fillRect(0, 0, 2 * R, 2 * R);
+    context.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
   }
 };
