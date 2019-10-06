94d9fe338f77fc37da12d84efb49d0c54f85cdaa
tama <tama@ttk1.net>
2019-10-07 00:41:20 +0900

    とりあえずできた。

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,28 @@
-const WIDTH = 500;
-const HEIGHT = 500;
 const POINT_SIZE = 1;
+const R = 300; // 外側の円の半径
+const r = 0.3; // 外側の円に対する、内側の円の半径の比
+const s = 0.8; // 内側の円の鉛筆を刺す位置（0が中心で、1が一番外側）
 
 window.onload = () => {
   const canvas = document.createElement('canvas');
-  canvas.width = WIDTH;
-  canvas.height = HEIGHT;
+  canvas.width = 2 * R;
+  canvas.height = 2 * R;
   document.body.appendChild(canvas);
   const context = canvas.getContext('2d');
 
   refresh();
-  for (let theta = 0; theta < 20 * Math.PI; theta += 0.001) {
-    drawPoint((1 + Math.sin(theta * 1.7)) / 2, theta);
-  }
+  let t = 0;
+  setInterval(() => {
+    drawPoint(t);
+    t += 0.01;
+  }, 5);
 
-  function drawPoint(r: number, theta: number) {
+  function drawPoint(theta: number) {
     context.fillStyle = 'black';
     context.beginPath();
     context.arc(
-      WIDTH * (1 + r * Math.cos(theta)) / 2,
-      HEIGHT * (1 + r * Math.sin(theta)) / 2,
+      R + R * (1 - r) * Math.cos(theta) + R * r * s * Math.cos(theta / r),
+      R + R * (1 - r) * Math.sin(theta) + R * r * s * Math.sin(theta / r),
       POINT_SIZE / 2, 0, 2 * Math.PI, true
     );
     context.fill();
@@ -27,6 +30,6 @@ window.onload = () => {
 
   function refresh() {
     context.fillStyle = 'white';
-    context.fillRect(0, 0, WIDTH, HEIGHT);
+    context.fillRect(0, 0, 2 * R, 2 * R);
   }
 };
